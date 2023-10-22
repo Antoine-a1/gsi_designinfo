cecbb4608867d0fb58d27847683bae86696fc398
Antoine-a1 <102758103+Antoine-a1@users.noreply.github.com>
2023-10-22 21:47:02 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const images = document.querySelectorAll('.carousel img');
+let index = 0;
 
+function changeImage() {
+  images[index].classList.remove('active');
+  index = (index + 1) % images.length;
+  images[index].classList.add('active');
+}
+
+setInterval(changeImage, 3000);
